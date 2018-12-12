75c2b135cb7966f95a2259289e6cad58d11d28e3
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-12-12 17:35:44 +0530

    Add validator to data schema

---
+++ etl/fish/script.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from ..schema_validator import utils
+import os
+
+dir_path = os.path.join(os.path.dirname(__file__))
+df = pd.read_excel(os.path.join(dir_path, 'fish.xlsx'))
+
+taxanomy = utils.taxanomy(
+    kingdom=df['Kingdom'],
+    phylum=df['Phylum'],
+    class_name=df['Class'],
+    order=df['Order'],
+    family=df['Family'],
+    species_authority=df['Species Authority:'],
+    infra_specific_taxa_assessed=df['Infra-specific Taxa Assessed:'],
+    common_name=df['Common Name/s: '],
+    synonyms=df['Synonym/s:'],
+    taxonomic_notes=df['Taxonomic Notes:'],
+    genus=df['Scientific Name:'].str.split().apply(lambda x: x[0]),
+    species=df['Scientific Name:'].str.split().apply(lambda x: x[1]))
+
+assessment_information = utils.assessment_information(
+    redlist_category=df['Red List Category & Criteria:'],
+    date_assessed=df['Year Assessed:'],
+    contributers=df['Contributor/s:'],
+    reviewers=df['Reviewer/s:'],
+    assessors=df['Assessor/s:'],
+    justification=df['Justification:'],
+    previously_published_red_list_assessments=df['History:'])
+
+geographic_range = utils.geographic_range(
+    range_description=df['Range Description:'],
+    countries_occurence=df['Countries:'],
+    fao_marine_fishing_areas=df['FAO Marine Fishing Areas:'],
+    endemic_status=df['Endemic/Not'])
+
+population = utils.population(
+    population=df['Population:'],
+    current_population_trend=df['Population Trend:'])
+
+habit_and_ecology = utils.habitat_and_ecology(
+    habitat_and_ecology=df['Habitat and Ecology:'],
+    systems=df['Systems:'],
+    list_of_habitats=df['List of Habitats:'])
+
+threats = utils.threats_and_major_threats(
+    threats_and_major_threats=df['Major Threat(s):'],
+    list_of_threats=df['List of Threats:'])
+
+conservation_actions = utils.conservation_actions(
+    conservation_actions=df['Conservation Actions:'],
+    list_of_conservation_actions=df['List of Conservation Actions:'])
+
+bibliography = utils.bibliography(
+    bibliography=df['Bibliography:'])
+
+citation = utils.citation(
+    citation=df['Citation:'])
+
+formed_data = [taxanomy, assessment_information, geographic_range, population, habit_and_ecology, threats,
+               conservation_actions, bibliography, citation]
+
+utils.validate_print_result(formed_data=formed_data, num_rows=34, path=dir_path)
---
+++ etl/schema_validator/utils.py
@@ -0,0 +1,126 @@
+import pandas as pd
+from collections import namedtuple
+
+from itertools import chain
+import functools
+import os
+
+taxanomy_fields = ('kingdom',
+                   'phylum',
+                   'class_name',
+                   'order',
+                   'family',
+                   'genus',
+                   'species',
+                   'species_authority',
+                   'infra_specific_taxa_assessed',
+                   'common_name',
+                   'synonyms',
+                   'taxonomic_notes')
+
+assessment_information_fields = ('redlist_category',
+                                 'criteria',
+                                 'date_assessed',
+                                 'year_published',
+                                 'contributers',
+                                 'reviewers',
+                                 'assessors',
+                                 'justification',
+                                 'previously_published_red_list_assessments',
+                                 'reasons_for_change',
+                                 'regional_assessment_information',
+                                 'national_status',
+                                 'comments')
+
+geographic_range_fields = ('range_description',
+                           'aoo',
+                           'eoo',
+                           'location',
+                           'elevation',
+                           'countries_occurence',
+                           'range_map',
+                           'fao_marine_fishing_areas',
+                           'endemic_status')
+
+population_fields = ('population',
+                     'current_population_trend',
+                     'year_of_population_estimate',
+                     'mature_individuals',
+                     'severely_fragmented',
+                     'number_of_subpopulation',
+                     'population_reduction_gen_time',
+                     'generation_time')
+
+habitat_and_ecology_fields = ('habitat_and_ecology',
+                              'systems',
+                              'list_of_habitats',
+                              'habit',
+                              'niche',
+                              'habitat_status')
+
+threats_and_major_threats_fields = ('threats_and_major_threats',
+                                    'list_of_threats',
+                                    'trade')
+
+stresses_fields = ('stresses',)
+
+conservation_actions_fields = ('conservation_actions',
+                               'presence_in_protected_areas',
+                               'list_of_conservation_actions',
+                               'conservation_needed',
+                               'research_needed',
+                               'ecosystem_services')
+
+bibliography_fields = ('bibliography',)
+citation_fields = ('citation',)
+
+taxanomy = namedtuple('taxanomy', taxanomy_fields, defaults=(None,) * len(taxanomy_fields), rename=True)
+assessment_information = namedtuple('assessment_information', assessment_information_fields,
+                                    defaults=(None,) * len(assessment_information_fields))
+geographic_range = namedtuple('geographic_range', geographic_range_fields,
+                              defaults=(None,) * len(geographic_range_fields))
+population = namedtuple('population', population_fields, defaults=(None,) * len(population_fields))
+habitat_and_ecology = namedtuple('habitat_and_ecology', habitat_and_ecology_fields,
+                                 defaults=(None,) * len(habitat_and_ecology_fields))
+threats_and_major_threats = namedtuple('threats_and_major_threats', threats_and_major_threats_fields,
+                                       defaults=(None,) * len(threats_and_major_threats_fields))
+stresses = namedtuple('stresses', stresses_fields, defaults=(None,) * len(stresses_fields))
+conservation_actions = namedtuple('conservation_actions', conservation_actions_fields,
+                                  defaults=(None,) * len(conservation_actions_fields))
+bibliography = namedtuple('bibliography', bibliography_fields, defaults=(None,) * len(bibliography_fields))
+citation = namedtuple('citation', citation_fields, defaults=(None,) * len(citation_fields))
+
+first_level_header = [taxanomy, assessment_information, geographic_range, population, habitat_and_ecology,
+                      threats_and_major_threats, stresses, conservation_actions, bibliography]
+
+
+def validate_dataframe(named_tuples, num):
+    count = functools.reduce(lambda x, y: x + y,
+                             chain.from_iterable(
+                                 map(lambda header:
+                                     map(lambda field: int(getattr(header, field) is not None),
+                                         header._fields), named_tuples)))
+
+    print("validation ", "passed" if count == num else "failed", " with ", count)
+
+    return count == num
+
+
+def form_data_frame(named_tuples):
+    column_header = list(chain.from_iterable(
+        map(lambda fields: fields, map(lambda header: map(lambda field: field, header._fields), first_level_header))))
+    df = pd.DataFrame(columns=column_header)
+    for header in named_tuples:
+        for field in header._fields:
+            if getattr(header, field) is None:
+                df[field] = ""
+            else:
+                df[field] = getattr(header, field)
+
+    return df
+
+
+def validate_print_result(formed_data, num_rows, path):
+    return form_data_frame(formed_data).to_csv(os.path.join(path, "result.csv"),
+                                               encoding='utf-8') if validate_dataframe(formed_data,
+                                                                                       num_rows) else "error"
