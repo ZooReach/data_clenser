e03c9f0918543a51adfa485206f2dca6db83db82
Shibi Chakravarthi <shibicr93@gmail.com>
2019-01-18 15:00:53 +0530

    added category detail for reptiles

--- etl/reptiles/addCategory.py
+++ etl/reptiles/addCategory.py
@@ -15,7 +15,8 @@ with open(os.path.join(dir_path, 'schema.json')) as f:
 for hierarchy in data.keys():
     group_level_data = data[hierarchy]
     for familyName in group_level_data.keys():
-        df.loc[df["genus             "].str.upper() == familyName.upper(), "category_level1"] = hierarchy
-        df.loc[df["genus             "].str.upper() == familyName.upper(), "category_level2"] = group_level_data[
+        df.loc[df["genus             "].str.strip().str.upper() == familyName.upper(), "category_level1"] = hierarchy
+        df.loc[df["genus             "].str.strip().str.upper() == familyName.upper(), "category_level2"] = \
+        group_level_data[
             familyName]
 df.to_csv("test.csv", encoding='utf-8', index=False)
